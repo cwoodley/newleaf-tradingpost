9c7354a3f3296523c9f9ece833fb9cb81a45a6c0
Cale Woodley <cale@cobalt343.com>
2013-06-06 13:59:18 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradingpost::Application.config.secret_token = '0fa01d5d53eba4aab80ba7f45eeb7cca73509d3f540e37050edc7bf6ec8abb5a189c6dd8e9ded1b752a3b7ffba7bd8c8d385722898090f287fa1a34eb4edaa98'
