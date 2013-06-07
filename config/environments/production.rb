fb9d5cee304f15d55651db7495dd002a3ca55117
Cale Woodley <cale@cobalt343.com>
2013-06-07 14:57:25 +0800

    disabled SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Tradingpost::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, 
   # and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
