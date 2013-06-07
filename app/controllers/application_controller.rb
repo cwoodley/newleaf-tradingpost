a46a61219015fa44dadfbb7425ef6d39379b1ee3
Cale Woodley <cale@cobalt343.com>
2013-06-07 15:09:43 +0800

    didnt work -_-

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :https_only_for_devise
 
   rescue_from CanCan::AccessDenied do |exception|
   	flash[:error] = "Access denied!"
     redirect_to root_url
   end  
-  
-  def https_only_for_devise
-    if (@@ssl_controllers.index(params[:controller]) == nil && request.ssl?)
-      redirect_to :protocol => 'http://'
-    end
-  end
 end
